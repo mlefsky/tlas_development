f2e7dfd124f99ef568b1f2ee0154541fecd21cf8
mlefsky <152317217+mlefsky@users.noreply.github.com>
2024-01-09 14:20:25 -0700

    Recommit
    After moving to teren notebook

---
+++ outstruct_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  3 14:21:09 2024
+
+@author: lefsky
+"""
+
+outstruct={"lasinputs":lasinputs,
+    "od_metrics":od_metrics,
+    "stackheight": stackheight,
+    "xysize":xysize,
+    "verticalres":1,
+    "groundthreshold":0.25,
+    "heightcol":'HeightAboveGround',
+    "method" : "gauss1d",
+    "sigma":0.5,
+    "rh":0.9,
+    "nbins":nbins,
+    "heightbins":heightbins}
+    
